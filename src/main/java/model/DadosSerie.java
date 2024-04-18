c50f7f5f243a6f2f602070b1f71d7af21fb4e120
Francisco Zitelli <francisco.junior@oi.net.br>
2024-04-18 14:31:33 -0300

    Inicio do projeto Spring

---
+++ src/main/java/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporada,
+                         @JsonAlias("imdbRating") String avaliacao) {
+
+}
